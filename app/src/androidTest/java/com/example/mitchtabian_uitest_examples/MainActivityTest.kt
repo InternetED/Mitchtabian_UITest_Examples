f8fff282b0ff86887505a8ac708ab17c3f28e2bc
ED <salahayo3192@gmail.com>
2020-04-06 14:25:21 +0800

    2-Activity Navigation Testing

--- app/src/androidTest/java/com/example/mitchtabian_uitest_examples/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/mitchtabian_uitest_examples/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.example.mitchtabian_uitest_examples
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -41,6 +42,34 @@ class MainActivityTest {
         onView(withId(R.id.activity_main_title))
             .check(matches(withText(R.string.text_main_activity)))
 
+    }
+
+    @Test
+    fun test_navSecondaryActivity() {
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.btnNextActivity)).perform(ViewActions.click())
+
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun test_backPress_toMainActivity() {
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.btnNextActivity)).perform(ViewActions.click())
+
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.btnBack)).perform(ViewActions.click()) // method 1
+
+//        pressBack() // method 2
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
 
     }
+
 }
